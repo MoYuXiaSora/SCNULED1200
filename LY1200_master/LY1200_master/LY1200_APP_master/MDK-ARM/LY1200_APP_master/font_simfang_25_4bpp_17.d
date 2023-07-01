b1c6bc1ce954a3ae31de7118618331114ff6e660
wenerooppss <1784623919@qq.com>
2023-07-01 19:43:57 +0800

    删除多余cct

---
+++ LY1200_master/LY1200_master/LY1200_APP_master/MDK-ARM/LY1200_APP_master/font_simfang_25_4bpp_17.d
@@ -0,0 +1,8 @@
+ly1200_app_master/font_simfang_25_4bpp_17.o: \
+  ..\touchgfx\generated\fonts\src\font_simfang_25_4bpp_17.cpp \
+  ..\middlewares\st\touchgfx\framework\include\touchgfx\hal\Types.hpp \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\stdint.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__config \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\middlewares\st\touchgfx\framework\include\touchgfx\hal\Config.hpp
